9e0e07019eb6586748b34620e1f9caa7dcb69239
smartcatboy <smartcatboy@tuta.io>
2022-02-04 21:50:40 +0800

    fix: brook tunnel client

--- src/components/RuleEditors/BrookRuleEditor.js
+++ src/components/RuleEditors/BrookRuleEditor.js
@@ -7,10 +7,10 @@ import { createForwardRule, editForwardRule } from "../../redux/actions/ports";
 
 const CommandOptions = [
   { label: "端口转发", value: "relay" },
-  { label: "server", value: "server" },
-  { label: "client", value: "client" },
-  { label: "wsserver", value: "wsserver" },
-  { label: "wsclient", value: "wsclient" },
+  { label: "普通隧道客户端（入口）", value: "client" },
+  { label: "普通隧道服务端（出口）", value: "server" },
+  { label: "ws 隧道客户端（入口）", value: "wsclient" },
+  { label: "ws 隧道服务端（出口）", value: "wsserver" },
 ];
 
 const BrookRuleEditor = ({
@@ -25,6 +25,8 @@ const BrookRuleEditor = ({
   const [command, setCommand] = useState("relay");
   const [remoteAddress, setRemoteAddress] = useState("");
   const [remotePort, setRemotePort] = useState(0);
+  const [serverAddress, setServerAddress] = useState("");
+  const [serverPort, setServerPort] = useState(0);
   const [password, setPassword] = useState("");
   const validRemoteAddress = useCallback(() => remoteAddress.length > 0, [
     remoteAddress,
@@ -32,6 +34,13 @@ const BrookRuleEditor = ({
   const validRemotePort = useCallback(
     () => parseInt(remotePort, 10) > 0 && parseInt(remotePort, 10) < 65536,
     [remotePort]
+  );
+  const validServerAddress = useCallback(() => serverAddress.length > 0, [
+    serverAddress,
+  ]);
+  const validServerPort = useCallback(
+    () => parseInt(serverPort, 10) > 0 && parseInt(serverPort, 10) < 65536,
+    [serverPort]
   );
   const validPassword = useCallback(() => password.length > 0, [password]);
 
@@ -43,10 +52,16 @@ const BrookRuleEditor = ({
       (command === "relay" || command === "client" || command === "wsclient"
         ? validRemotePort()
         : true) &&
+      (command === "client" || command === "wsclient"
+        ? validServerAddress()
+        : true) &&
+      (command === "client" || command === "wsclient"
+        ? validServerPort()
+        : true) &&
       command !== "relay"
         ? validPassword()
         : true,
-    [command, validRemoteAddress, validRemotePort, validPassword]
+    [command, validRemoteAddress, validRemotePort, validServerAddress, validServerPort, validPassword]
   );
   const submitRuleForm = useCallback(() => {
     const data = {
@@ -55,6 +70,8 @@ const BrookRuleEditor = ({
         command,
         remote_address: remoteAddress,
         remote_port: remotePort,
+        server_address: serverAddress,
+        server_port: serverPort,
         password,
       },
     };
@@ -71,6 +88,8 @@ const BrookRuleEditor = ({
     command,
     remoteAddress,
     remotePort,
+    serverAddress,
+    serverPort,
     password,
     forwardRule,
   ]);
@@ -85,15 +104,23 @@ const BrookRuleEditor = ({
       if (forwardRule.config.remote_port)
         setRemotePort(forwardRule.config.remote_port);
       else setRemotePort(0);
+      if (forwardRule.config.server_address)
+        setServerAddress(forwardRule.config.server_address);
+      else setServerAddress("");
+      if (forwardRule.config.server_port)
+        setServerPort(forwardRule.config.server_port);
+      else setServerPort(0);
       if (forwardRule.config.password) setPassword(forwardRule.config.password);
       else setPassword("");
     } else {
       setCommand("relay");
       setRemoteAddress("");
       setRemotePort(0);
+      setServerAddress("");
+      setServerPort(0);
       setPassword("");
     }
-  }, [forwardRule, setCommand, setRemoteAddress, setRemotePort, setPassword]);
+  }, [forwardRule, setCommand, setRemoteAddress, setRemotePort, setServerAddress, setServerPort, setPassword]);
 
   useEffect(() => {
     if (method === "brook") {
@@ -126,7 +153,7 @@ const BrookRuleEditor = ({
       </Label>
       {command === "relay" || command === "client" || command === "wsclient" ? (
         <Label className="mt-4">
-          <span>{command === "relay" ? "远端" : "Brook服务器"}IP</span>
+          <span>目标地址</span>
           <Input
             className="mt-1"
             placeholder="1.1.1.1"
@@ -138,13 +165,37 @@ const BrookRuleEditor = ({
       ) : null}
       {command === "relay" || command === "client" || command === "wsclient" ? (
         <Label className="mt-4">
-          <span>{command === "relay" ? "远端" : "Brook服务器"}端口</span>
+          <span>目标端口</span>
           <Input
             className="mt-1"
             placeholder="8888"
             value={remotePort}
             valid={validRemotePort()}
             onChange={(e) => setRemotePort(e.target.value)}
+          />
+        </Label>
+      ) : null}
+      {command === "client" || command === "wsclient" ? (
+        <Label className="mt-4">
+          <span>Brook 服务端地址</span>
+          <Input
+            className="mt-1"
+            placeholder="1.1.1.1"
+            value={serverAddress}
+            valid={validServerAddress()}
+            onChange={(e) => setServerAddress(e.target.value)}
+          />
+        </Label>
+      ) : null}
+      {command === "client" || command === "wsclient" ? (
+        <Label className="mt-4">
+          <span>Brook 服务端端口</span>
+          <Input
+            className="mt-1"
+            placeholder="8888"
+            value={serverPort}
+            valid={validServerPort()}
+            onChange={(e) => setServerPort(e.target.value)}
           />
         </Label>
       ) : null}
