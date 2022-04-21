35809356c4d4cf9b9cf2357588c9d42444ca26f4
Lei Shi <me@leishi.io>
2022-04-21 22:43:11 +0200

    support realm v2

--- src/components/RuleEditors/RealmRuleEditor.js
+++ src/components/RuleEditors/RealmRuleEditor.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { useDispatch } from "react-redux";
 
-import { Input, Label } from "@windmill/react-ui";
+import { Input, Label, Select } from "@windmill/react-ui";
 
 import { createForwardRule, editForwardRule } from "../../redux/actions/ports";
 
+const CommandOptions = [
+  { label: "TCP - 普通端口转发", value: "tcp" },
+  { label: "WS - 入口", value: "ws-in" },
+  { label: "WS - 出口", value: "ws-out" },
+  { label: "WSS - 入口", value: "wss-in" },
+  { label: "WSS - 出口", value: "wss-out" },
+];
 
 const RealmRuleEditor = ({
   serverId,
@@ -15,6 +22,7 @@ const RealmRuleEditor = ({
   setSubmitRuleForm,
 }) => {
   const dispatch = useDispatch();
+  const [command, setCommand] = useState("tcp");
   const [remoteAddress, setRemoteAddress] = useState("");
   const [remotePort, setRemotePort] = useState(0);
   const validRemoteAddress = useCallback(() => remoteAddress.length > 0, [
@@ -33,6 +41,7 @@ const RealmRuleEditor = ({
     const data = {
       method,
       config: {
+        command,
         remote_address: remoteAddress,
         remote_port: remotePort,
       },
@@ -47,6 +56,7 @@ const RealmRuleEditor = ({
     serverId,
     portId,
     method,
+    command,
     remoteAddress,
     remotePort,
     forwardRule,
@@ -54,6 +64,8 @@ const RealmRuleEditor = ({
 
   useEffect(() => {
     if (forwardRule) {
+      if (forwardRule.config.command) setCommand(forwardRule.config.command);
+      else setCommand("tcp");
       if (forwardRule.config.remote_address)
         setRemoteAddress(forwardRule.config.remote_address);
       else setRemoteAddress("");
@@ -61,10 +73,11 @@ const RealmRuleEditor = ({
         setRemotePort(forwardRule.config.remote_port);
       else setRemotePort(0);
     } else {
+      setCommand("tcp");
       setRemoteAddress("");
       setRemotePort(0);
     }
-  }, [forwardRule, setRemoteAddress, setRemotePort]);
+  }, [forwardRule, setCommand, setRemoteAddress, setRemotePort]);
 
   useEffect(() => {
     if (method === "realm") {
@@ -73,8 +86,6 @@ const RealmRuleEditor = ({
     }
   }, [
     method,
-    remoteAddress,
-    remotePort,
     setValidRuleForm,
     setSubmitRuleForm,
     validRuleForm,
@@ -84,25 +95,39 @@ const RealmRuleEditor = ({
   return (
     <>
       <Label className="mt-4">
-        <span>远端地址</span>
-        <Input
+        <span>传输协议</span>
+        <Select
           className="mt-1"
-          placeholder="www.example.com"
-          value={remoteAddress}
-          valid={validRemoteAddress()}
-          onChange={(e) => setRemoteAddress(e.target.value)}
-        />
+          value={command}
+          onChange={(e) => setCommand(e.target.value)}
+        >
+          {CommandOptions.map((option) => (
+            <option value={option.value} key={`brook_command_${option.value}`}>
+              {option.label}
+            </option>
+          ))}
+        </Select>
       </Label>
-      <Label className="mt-4">
-        <span>远端端口</span>
-        <Input
-          className="mt-1"
-          placeholder={0}
-          value={remotePort}
-          valid={validRemotePort()}
-          onChange={(e) => setRemotePort(e.target.value)}
-        />
-      </Label>
+        <Label className="mt-4">
+          <span>{command.endsWith("in") ? "出口" : "落地"}地址</span>
+          <Input
+            className="mt-1"
+            placeholder="1.1.1.1"
+            value={remoteAddress}
+            valid={validRemoteAddress()}
+            onChange={(e) => setRemoteAddress(e.target.value)}
+          />
+        </Label>
+        <Label className="mt-4">
+          <span>{command.endsWith("in") ? "出口" : "落地"}端口</span>
+          <Input
+            className="mt-1"
+            placeholder="8888"
+            value={remotePort}
+            valid={validRemotePort()}
+            onChange={(e) => setRemotePort(e.target.value)}
+          />
+        </Label>
     </>
   );
 };
